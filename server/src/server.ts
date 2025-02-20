77f5eca8dac838108173c583ccb89c584f9c4983
Ismenia Cristina <snowismscris@gmail.com>
2025-02-20 12:03:59 -0300

    aula02

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,7 @@ import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi} from '@fastify/swagger-ui'
 import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 import { env } from './env'
+import { accessInviteLinkRoute } from './routes/access-invite-links'
 
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -36,6 +37,7 @@ app.register (fastifySwaggerUi,{
 })
 
 app.register(subscribeToEventRoute)
+app.register(accessInviteLinkRoute)
 
 app.listen({ port: env.PORT}).then(()=>{
     console.log('HTTP server running!')
